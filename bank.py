eaa3101c73698ebff418a7f8a661362049d2e59d
cgroark <colingroark@gmail.com>
2018-02-06 17:02:30 -0800

    done with homework

--- bank.py
+++ bank.py
@@ -1,11 +1,49 @@
 class BankAccount:
-  pass
+	def __init__(self):
+		self.balance = 0
+		self.interest = 0.02
 
-class ChildrensAccount:
-  pass
+	def deposit(self, amount):
+		if amount < 0:
+			return False
+		else:
+			self.balance += amount
+			return self.balance
 
-class OverdraftAccount:
-  pass
+	def withdraw(self, amount):
+		if amount < 0:
+			return False
+		else: 
+			self.balance -= amount
+			return self.balance
+
+	def accumulate_interest(self):
+		self.balance += (self.balance * self.interest)
+		return self.balance
+
+class ChildrensAccount(BankAccount):
+	def __init__(self):
+		super().__init__()
+		self.interest = 10
+	
+	def accumulate_interest(self):
+		self.balance += (self.balance + self.interest)
+		return self.balance
+
+class OverdraftAccount(BankAccount):
+	def __init__(self):
+		super().__init__()
+		self.overdraft_penalty = 40
+		self.interest = 0
+  
+	def withdraw(self, amount):
+		if amount > self.balance:
+			self.balance -= self.overdraft_penalty
+			return False
+		else:
+			self.balance -= amount
+			return self.balance
+
 
 basic_account = BankAccount()
 basic_account.deposit(600)
